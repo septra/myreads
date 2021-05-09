8b95322a4d54af24d474ed4274172e06229ea8e0
Sahil Singh <sahilsingh90@gmail.com>
2021-05-09 21:01:11 +0530

    Implement component for a Shelf

---
+++ src/Shelf.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import Book from './Book.js'
+
+class Shelf extends Component {
+    render () {
+
+        const { title, books } = this.props
+
+        return (
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">{title}</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books.map((book) => ( 
+                        <li key={book.title}>
+                            <Book book={book} />
+                        </li>
+                    ))}
+                </ol>
+              </div>
+            </div>
+        )
+    }
+}
+
+export default Shelf

f523a00fc18cc46b684a17faf9fc3eb7e3910a0e
Sahil Singh <sahilsingh90@gmail.com>
2021-05-11 00:21:20 +0530

    Pre-select correct option values for books assigned to shelves

--- src/Book.js
+++ src/Book.js
@@ -6,6 +6,11 @@ class Book extends Component {
 
         const { book } = this.props
 
+        const read = 'read' === book.shelf
+        const wantToRead = 'wantToRead' === book.shelf
+        const currenlyReading = 'currentlyReading' === book.shelf
+        const none = !read && !wantToRead && !currenlyReading
+
         return (
             <div className="book">
               <div className="book-top">
@@ -16,11 +21,11 @@ class Book extends Component {
                 }}></div>
                 <div className="book-shelf-changer">
                   <select>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
+                    <option value="move" disabled >Move to...</option>
+                    <option value="currentlyReading" selected={currenlyReading}>Currently Reading</option>
+                    <option value="wantToRead" selected={wantToRead}>Want to Read</option>
+                    <option value="read" selected={read}>Read </option>
+                    <option value="none" selected={none}>None</option>
                   </select>
                 </div>
               </div>
